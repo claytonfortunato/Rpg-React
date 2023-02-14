5aa18382e31ade97cab608617a665cb0a5691f33
claytonfortunato <claytonreis10@gmail.com>
2023-02-14 13:10:16 -0300

    feat: mover o char

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -8,5 +8,9 @@ type Props = {
 export const Character = ({ x, y }: Props) => {
   const size = 30;
 
-  return <C.Container size={size} left={x * size} top={y * size}></C.Container>;
+  return (
+    <C.Container size={size} left={x * size} top={y * size}>
+      ...
+    </C.Container>
+  );
 };
--- src/components/Character/styles.ts
+++ src/components/Character/styles.ts
@@ -5,10 +5,10 @@ export const Container = styled.div<{
   left: number;
   top: number;
 }>`
-  width: ${(props) => props.size};
-  height: ${(props) => props.size};
-  background-color: #ff0000;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  background-color: red;
   position: absolute;
-  left: ${(props) => props.left};
-  top: ${(props) => props.top};
+  left: ${(props) => props.left}px;
+  top: ${(props) => props.top}px;
 `;
