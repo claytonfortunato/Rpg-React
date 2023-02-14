f011c5ab7a3c1a02af824430ab2380a63d115d91
claytonfortunato <claytonreis10@gmail.com>
2023-02-14 13:42:03 -0300

    feat: direcionando o char

--- src/components/Character/styles.ts
+++ src/components/Character/styles.ts
@@ -4,11 +4,13 @@ export const Container = styled.div<{
   size: number;
   left: number;
   top: number;
+  sidePos: number;
 }>`
   width: ${(props) => props.size}px;
   height: ${(props) => props.size}px;
-  background-color: red;
   position: absolute;
   left: ${(props) => props.left}px;
   top: ${(props) => props.top}px;
+  background-image: url("/assets/char.png");
+  background-position: 0px ${(props) => props.sidePos}px;
 `;
