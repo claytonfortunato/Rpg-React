610b80f5db34cedb6813c38554cc9485acab0442
claytonfortunato <claytonreis10@gmail.com>
2023-02-13 23:29:15 -0300

    feat: character

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import * as C from "./App.styles";
+import { Character } from "./components/Character";
 
 const App = () => {
   return (
     <C.Container>
-      <C.Map></C.Map>
+      <C.Map>
+        <Character x={30} y={30} />
+      </C.Map>
     </C.Container>
   );
 };
---
+++ src/components/Character/index.tsx
@@ -0,0 +1,10 @@
+import * as C from "./styles";
+
+type Props = {
+  x: number;
+  y: number;
+};
+
+export const Character = ({ x, y }) => {
+  return <C.Container>...</C.Container>;
+};
